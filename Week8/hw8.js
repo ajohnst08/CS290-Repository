cee5784997f3897863d60e1007cab36452dc503f
Allie <ajohnst08@gmail.com>
2016-02-28 15:42:05 -0500

    newweek

---
+++ Week8/hw8.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  var context = {};
+  if (req.body['submit']){
+	  req.session.city = req.body.city;
+	  req.session.zip = req.body.zip;
+	request('http://api.openweathermap.org/data/2.5/weather?q=' + req.session.city + '&APPID=29568ddaed32e25923f823b59fc4899a' + '&units=imperial', function(err,response,body)){
+	   if(!err && response.statusCode < 400){
+		context.owm = body;
+		res.render('home', context);
+	   }
+	   else {
+		   console.log(err);
+		   if(response){
+			   console.log(response.statusCode);
+		   }
+		   next(err);
+	   }
+	}
+  }
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
