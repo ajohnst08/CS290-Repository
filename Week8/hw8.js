cd2c8747121bf6ba654f570e625182d1151ff19a
Allie <ajohnst08@gmail.com>
2016-02-28 20:37:47 -0500

    ppost2

--- Week8/hw8.js
+++ Week8/hw8.js
@@ -13,10 +13,13 @@ app.set('view engine', 'handlebars');
 app.set('port', 3000);
 app.use(session({secret:'sec'}));
 
-app.get('/',function(req,res){
+app.post('/',function(req,res){
   var context = {};
 
-	request('http://api.openweathermap.org/data/2.5/weather?q=corvallis&APPID=29568ddaed32e25923f823b59fc4899a&units=imperial', function(err,response,body){
+	request({"url":"http://api.openweathermap.org/data/2.5/weather?q=corvallis&APPID=29568ddaed32e25923f823b59fc4899a&units=imperial",
+	"method":"POST",
+	"headers":{"Content-Type":"application/json"}
+	}, function(err,response,body){
 	   if(!err && response.statusCode < 400){
 		context.owm = 'temperature = ' + JSON.parse(body).main.temp;
 		console.log(context.owm);
