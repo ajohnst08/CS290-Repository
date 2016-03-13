51ed3cb7a2baf252ff0b279e9e3e1e143a19e7cd
Allie <ajohnst08@gmail.com>
2016-03-13 15:39:41 -0400

    db1

---
+++ Week10/hw10.js
@@ -0,0 +1,58 @@
+var express = require ('express');
+var handlebars = require ('express-handlebars').create({defaultLayout:'main'});
+var mysql = require('mysql');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port',3000);
+
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'student',
+	password: 'default',
+	database: 'student'
+});
+
+app.get('/',function(req,res,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.render('home', context);
+  });
+});
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+    var createString = "CREATE TABLE todo(" +
+    "id INT PRIMARY KEY AUTO_INCREMENT," +
+    "name VARCHAR(255) NOT NULL," +
+    "done BOOLEAN," +
+    "due DATE)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
